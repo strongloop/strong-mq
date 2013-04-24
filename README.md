0e19f8c6686029f4456a6e2b7e7a6dfdf999d845
Sam Roberts <sam@strongloop.com>
2013-04-24 13:21:53 -0700

    Add test of connectivity to rabbitmq

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # clustermq - MQ API with cluster integration, implemented over various message queues
 
-...
+Run tests:
+
+    % make test
+
+Requires rabbitmq server running on localhost:
+
+    % rabbitmq-server
 
--- test/test.js
+++ test/test.js
@@ -11,6 +11,25 @@ describe("stub cluster test", function () {
   })
 })
 
+var amqp = require("amqp")
+
+describe("check for amqp connectivity", function () {
+  it("can create a queue with amqp", function (done) {
+    var connection = amqp.createConnection()
+    // console.log("connection created")
+    connection.on("ready", function () {
+      // console.log("connection ready")
+      connection.queue("my-queue", function (q) {
+        // console.log("my-queue ready")
+        q.destroy()
+        connection.end()
+        done()
+      })
+    })
+  })
+})
+
+
 describe("SomeTest", function () {
   it.skip("sync fail", function () {
     assert(false, "ok")
