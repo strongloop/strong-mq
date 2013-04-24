65eaf543f3f523836a1b660c727c43c61651155b
Sam Roberts <sam@strongloop.com>
2013-04-24 11:56:04 -0700

    Add async tests, and failing tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,21 @@
 assert = require("assert")
 
 describe("SomeTest", function () {
-  it("should pass", function () {
+  it.skip("sync fail", function () {
+    assert(false, "ok")
+  })
+  it.skip("leaks globals", function () {
+    bad_global = "OOPS"
+  })
+  it("sync pass", function () {
     assert(true, "ok")
+  })
+  it("async pass", function (done) {
+    process.nextTick(function () {
+      assert(true, "ok")
+      done()
+    })
+    console.log("passing to next tick")
   })
 })
 
