0c5ac605051d16f46484180bcde8d44fdc544f67
Sam Roberts <sam@strongloop.com>
2013-05-10 12:47:11 -0700

    Delayed jobs are done in series after start

---
+++ jobs.js
@@ -0,0 +1,33 @@
+// Jobs: job flow control
+var async = require('async');
+
+exports.delayed = delayed;
+
+// d = jobs.delayed([drained])
+//
+// call drained every time all jobs are done
+//
+// d.push(job)
+//
+// job is function(done), it should call done() when complete
+//
+// d.start()
+//
+// start processing queue
+function delayed(drained) {
+  var self = async.queue(function(task, callback) {
+    task(callback)
+  }, 0);
+  // XXX(sam) wrap .push() so it returns self?
+  self.start = function() {
+    self.concurrency = 1;
+    // Make async notice that concurrency has changed.
+    self.push(function primer(done) {
+      done();
+    });
+    return self;
+  };
+  self.drain = drained;
+  return self;
+}
+
---
+++ test/jobs.js
@@ -0,0 +1,45 @@
+var delayed = require('../jobs').delayed;
+var assert = require('assert');
+
+describe('job queue', function() {
+  it('should wait to start', function(done) {
+    var started = false;
+    var jobs = delayed(function() {
+      //console.log('drained');
+      assert(started);
+      done();
+    });
+
+    process.nextTick(function() {
+      jobs.push(function(callback) {
+        started = true;
+        //console.log('first job');
+        process.nextTick(callback);
+      });
+    });
+
+    process.nextTick(function() {
+      process.nextTick(function() {
+        assert(!started, 'two ticks later, still not started');
+        //console.log('before start');
+        jobs.start();
+        //console.log('after start');
+      });
+    });
+  });
+
+  it('should do jobs immediately after start', function(done) {
+    var started = false;
+    var jobs = delayed(function() {
+      assert(started);
+      done();
+    }).start();
+
+    jobs.push(function(callback) {
+      started = true;
+      callback();
+    });
+
+    assert(!started, 'pushed jobs are done in next tick');
+  });
+});
