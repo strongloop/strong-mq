cf3bb9430286c96563da8fcef9a2e674706213b8
Sam Roberts <sam@strongloop.com>
2013-05-16 15:59:39 -0700

    Topic matching for native fixes
    The '.' word seperator in topic patterns is a regex-special, so
    topics of aXbXc used to match patterns of a.b, when only exact
    prefix matching should have occurred. Topic matching is now
    in lib/topic.js.

--- test/topic.js
+++ test/topic.js
@@ -25,7 +25,22 @@ describe('topic', function() {
   });
 
   it('should escape dots for use in regular expressions', function() {
-    assert.equal(topic.escape('a.b.c'), 'a\\.b\\.c');
+    assert.equal(topic._escape('a.b.c'), 'a\\.b\\.c');
+  });
+
+  it('should match identical prefixes', function() {
+    assert(topic.matcher('a').test('a'));
+    assert(topic.matcher('a').test('a.c'));
+
+    assert(!topic.matcher('a').test('ab'));
+
+
+    assert(topic.matcher('a0_._9_').test('a0_._9_'));
+
+    assert(!topic.matcher('a0_z._9_').test('a0_._9_'));
+    assert(!topic.matcher('a0_._9_').test('a0_z._9_'));
+    assert(!topic.matcher('a0_._9_').test('a0_._9_z'));
+
   });
 
 });
